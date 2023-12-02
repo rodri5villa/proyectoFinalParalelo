08580cccf83eb810ec0c882b40805a0b5b6849a5
borjamfcayon <borja.martinez@alumnos.uneatlantico.es>
2023-12-02 20:21:05 +0100

    Repositorio y controlador de League

---
+++ FootballFinalProject/BackFinalProject/Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackFinalProject.Models
+{
+    public class Team
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public int leagueId { get; set; }
+    }
+}
