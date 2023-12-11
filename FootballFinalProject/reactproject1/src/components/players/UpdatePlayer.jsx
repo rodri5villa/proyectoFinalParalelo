86c8ca1d609d9b9ca107066daad951841f71d40d
borjamfcayon <borja.martinez@alumnos.uneatlantico.es>
2023-12-11 22:47:59 +0100

    030

--- FootballFinalProject/reactproject1/src/components/players/UpdatePlayer.jsx
+++ FootballFinalProject/reactproject1/src/components/players/UpdatePlayer.jsx
@@ -17,11 +17,22 @@ function UpdatePlayer() {
     const [pass, setPass] = useState("");
     const [teamId, setTeamId] = useState("");
     const [image, setImage] = useState(null);
+    const [tempImage, setTempImage] = useState(null);
     const [update, setUpdate] = useState(false);
 
     useEffect(() => {
         fetchData(playerApi.getById(id));
     }, [update]);
+
+    useEffect(() => {
+        setName(data.name || "");
+        setLastName(data.lastName || "");
+        setAttack(data.attack || "");
+        setDefense(data.defense || "");
+        setPass(data.pass || "");
+        setTeamId(data.teamId || "");
+        setImage(data.image);
+    },[data])
 
     useEffect(() => {
         if (data) {
@@ -35,20 +46,35 @@ function UpdatePlayer() {
         }
     }, [data]);
 
-    const handleUpdate = () => {
-        const updatedPlayer = {
-            id: parseInt(id, 10),
-            name: name,
-            lastName: lastName,
-            attack: attack,
-            defense: defense,
-            pass: pass,
-            teamId: teamId,
-            image: image,
-        };
+    const handleUpdate = async () => {
+        let updatedPlayer;
+        if (tempImage) {
+            const base64Image = await convertBlobToBase64(tempImage);
+            updatedPlayer = {
+                id: id,
+                name: name,
+                lastName: lastName,
+                attack: attack,
+                defense: defense,
+                pass: pass,
+                teamId: teamId,
+                image: base64Image,
+            };
+        } else {
+            updatedPlayer = {
+                id: id,
+                name: name,
+                lastName: lastName,
+                attack: attack,
+                defense: defense,
+                pass: pass,
+                teamId: teamId,
+                image: image,
+            };
+        }
 
-        fetchData(playerApi.update(id, updatedPlayer));
-        setUpdate(!update)
+        await fetchData(playerApi.update(id, updatedPlayer));
+        setUpdate(!update);
     }
 
     const handleDelete = () => {
@@ -65,6 +91,20 @@ function UpdatePlayer() {
         setTeamId(data.teamId || "");
         setImage("");
     }
+
+    const convertBlobToBase64 = (blob) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onloadend = () => {
+                const base64Image = reader.result.split(',')[1];
+                resolve(base64Image);
+            };
+            reader.onerror = (error) => {
+                reject(error);
+            };
+        });
+    };
 
     return (
         <>
@@ -90,8 +130,28 @@ function UpdatePlayer() {
                                 <input type="text" id="teamId" name="teamId" value={teamId} onChange={(e) => setTeamId(e.target.value)} />
                             </div>
                             <div className="mid-player">
-                                <img width="" alt="imagenJugador" />
-                                <div className="processPlayer">
+                                        <img
+                                            src={tempImage ? URL.createObjectURL(tempImage) : (image ? `data:image/png;base64,${image}` : null)}
+                                            width="200"
+                                            alt="  *Agregar Imagen"
+                                            onClick={() => document.getElementById('fileInput').click()}
+                                        />
+                                        <input
+                                            type="file"
+                                            id="fileInput"
+                                            style={{ display: 'none' }}
+                                            onChange={(e) => {
+                                                const file = e.target.files[0];
+                                                setTempImage(file);
+                                                const reader = new FileReader();
+                                                reader.onloadend = () => {
+                                                    setImage(reader.result);
+                                                };
+                                                if (file) {
+                                                    reader.readAsDataURL(file);
+                                                }
+                                            }}
+                                        />                                <div className="processPlayer">
                                     <button onClick={handleRestore}>Restaurar</button>
                                     <button className={"update-button"} onClick={handleUpdate}>Actualizar</button>
                                     <button className={"delete-button"} onClick={handleDelete}>Eliminar</button>
